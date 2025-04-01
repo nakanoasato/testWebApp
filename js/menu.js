f712762c151e106b60b9b771431e4bf14aee9cd9
anakano <nakano.asato@jp.panasonic.com>
2025-04-01 11:25:34 +0900

    3rd commit

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,17 @@
 // js/menu.js
-
 document.addEventListener("DOMContentLoaded", function () {
     const hamburger = document.getElementById("hamburger");
     const sideMenu = document.getElementById("side-menu");
   
     hamburger.addEventListener("click", () => {
       sideMenu.classList.toggle("open");
+    });
+  
+    // メニューを閉じるエリアクリック用（外側クリック時）
+    document.addEventListener("click", function (e) {
+      const isClickInside = hamburger.contains(e.target) || sideMenu.contains(e.target);
+      if (!isClickInside) {
+        sideMenu.classList.remove("open");
+      }
     });
   });
